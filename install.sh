74d1c93ae94f65e2f1915e436e19743afaf9b883
jianzchen <jianzchen@ebay.com>
2015-06-11 15:20:26 +0800

    install

---
+++ install.sh
@@ -0,0 +1,13 @@
+# JDK $JAVA_HOME ant git
+
+git clone git://github.com/playframework/play.git
+cd play/framework
+ant
+
+export PATH=~/play:$PATH
+
+play
+play help run
+play new myApp
+play run myApp
+http://localhost:9000
